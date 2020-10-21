ed26834a0203e83602a3bf699c6dc3908c6146a9
HEITOR DE SOUZA GARCIA <312250@fpte.local>
2020-10-21 12:52:15 -0300

    markers 21/10

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -12,6 +12,8 @@ export class Tab4Page implements OnInit {
   latitude: number = -21.6756695;
   longitude: number = -49.7537441;
   map:any;
+  layers:any;
+  marker:any;
   constructor() { }
 
   ngOnInit() {
@@ -27,6 +29,30 @@ export class Tab4Page implements OnInit {
         zoom: 20
       })
     });
+    this.layers = new ol.layer.Vector({
+      source:new ol.source.Vector(),
+      style: new ol.style.Style({
+        image: new ol.style.Icon({
+          anchor: [0.5, 1],
+          scale:1,
+          src:'./assets/icon/map-marker.png'
+        })
+      })
+        /*features:[
+          new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.fromLonLat([-49.75374415,-21.6756695]))
+          })
+        ]*/
+
+      
+    });
+
+    this.map.addLayer(this.layers);
   }
 
+  addMarker(){
+   this.marker = new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat([-49.75374415,-21.6756695])))
+   this.layers.getSource().addFeature(this.marker);
+
+  }
 }
